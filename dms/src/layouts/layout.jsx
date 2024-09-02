fcff10699899be386f0eaa92d3a7c77eaee455d5
arnavgour <133200768+arnavgour@users.noreply.github.com>
2024-09-02 14:04:52 +0530

    Created structured dms folder structure

---
+++ dms/src/layouts/layout.jsx
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+    return (
+        <div className={inter.className}>
+            {children}
+        </div>
+    );
+}
