a28c5418e960f3f090c61db727b0a50c60ac084c
arnavgour <133200768+arnavgour@users.noreply.github.com>
2024-09-03 15:23:28 +0530

    app folder removed

--- dms/src/pages/index.jsx
+++ dms/src/pages/index.jsx
@@ -1,8 +1,30 @@
-import Image from "next/image";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+
+// Dynamically import the Chart component to avoid SSR issues with ApexCharts
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 export default function Home() {
+  const [options, setOptions] = useState({
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  });
+
+  const [series, setSeries] = useState([
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91, 99],
+    },
+  ]);
+
   return (
-    <>
-    </>
+      <div className="app">
+          <Chart options={options} series={series} type="bar" width="500" />
+      </div>
   );
 }
