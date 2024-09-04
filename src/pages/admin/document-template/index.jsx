a1b20e2aab02756f6b7c287307bce77d6b7e71d0
sahu-ank <ankurmaildx@gmail.com>
2024-09-04 16:53:05 +0530

    document template created index files

---
+++ src/pages/admin/document-template/index.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import Head from "next/head";
+import Table from "../../../components/Table";
+import Button from "../../../components/Button";
+
+
+export default () => {
+
+  return (
+    <>
+      <Head>
+        <title>Document Template</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={"p-4 mx-2 mt-2 flex flex-col gap-4"}>
+        <h2 className={"text-2xl font-semibold text-primary-900 dark:text-primary-300"}>
+          Document Template
+        </h2>
+        <div className={"text-primary-900 dark:text-primary-200"}>
+          <Button
+            onClick={() => { }}
+            primary
+            size="large"
+            text="Create"
+          />
+        </div>
+
+        <div className={"mt-4"}>
+          <Table
+            rowId={"Id"}
+            rowName={"DisplayName"}
+            data={{
+              source: "https://jsonplaceholder.typicode.com/posts",
+            }}
+            columns={[
+              {
+                header: "user Id",
+                field: "userId",
+              },
+              {
+                header: "id",
+                field: "id",
+              },
+              {
+                header: "title",
+                field: "title",
+              },
+              {
+                header: "Content",
+                field: "body",
+              },
+            ]}
+            actions={[
+              {
+                label: "View",
+                //icon: faEye,
+                onClick: (active) => {
+                  router.push(
+                    {
+                      pathname: `/form/${active.name}`,
+                      query: {
+                        templateId: active.id,
+                      },
+                    },
+                    `/form/${active.name}`,
+                  );
+                },
+              },
+              {
+                label: "Edit",
+                // icon: faPencil,
+                onClick: (active) => {
+                  router.push(`/template/manage/${active.id}`);
+                },
+              },
+              {
+                label: "Delete",
+                // icon: faTrash,
+                onClick: (active) => {
+                  console.log("Delete");
+                },
+              },
+            ]}
+          ></Table>
+        </div>
+      </main>
+    </>
+  );
+}
