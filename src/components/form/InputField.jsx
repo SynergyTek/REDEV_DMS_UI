f4fbf112b09a600fdfd299ddbfc82c1608194ac4
Sudhanshu Modi <sudhanshu565868@gmail.com>
2024-09-09 15:35:17 +0530

    width full

--- src/components/form/InputField.jsx
+++ src/components/form/InputField.jsx
@@ -1,16 +1,16 @@
-import {useRef, useState} from "react";
-import {Button} from "~";
+import { useRef, useState } from "react";
+import { Button } from "~";
 
 function InputField({
-	                    id,
-	                    label,
-	                    type,
-	                    placeholder = "",
-	                    required = false,
-	                    button,
-	                    addon,
-	                    ...props
-                    }) {
+	id,
+	label,
+	type,
+	placeholder = "",
+	required = false,
+	button,
+	addon,
+	...props
+}) {
 	const [error, setError] = useState(null);
 	const inputRef = useRef();
 	const inputClass = ` w-full invalid:text-red-500 group-hover:text-primary-800 dark:group-hover:text-primary-200 text-primary-600 dark:text-secondary-300 focus:text-primary-800 dark:focus:text-primary-200 peer rounded border-none bg-inherit p-3 ${placeholder === "" ? "placeholder-transparent" : "placeholder-primary-300 placeholder-opacity-60 placeholder-text-sm"} focus:border-transparent focus:outline-none focus:ring-0 transition-all duration-300`;
@@ -30,12 +30,11 @@ function InputField({
 		}
 	};
 	return (
-		<div id={"input"}
-		     className={`flex gap-4 w-full ${props.className}`}>
+		<div id={"input"} className={`flex gap-4 w-full ${props.className}`}>
 			<div className={`flex w-full gap-2 ${error ? "mb-4" : ""}`}>
 				<div
 					className={
-						"group relative block rounded border border-primary-400 dark:border-secondary-700 bg-primary-50 shadow-sm hover:border-primary-600 dark:hover:border-primary-400 focus-within:border-primary-600 dark:focus-within:border-primary-400 focus-within:ring-0 dark:bg-gray-950 transition-all duration-300"
+						"group relative block rounded border border-primary-400 dark:border-secondary-700 bg-primary-50 shadow-sm hover:border-primary-600 dark:hover:border-primary-400 focus-within:border-primary-600 dark:focus-within:border-primary-400 focus-within:ring-0 dark:bg-gray-950 transition-all duration-300 w-full"
 					}
 				>
 					{type === "textarea" ? (
@@ -51,7 +50,7 @@ function InputField({
 								validateInput();
 							}}
 							onBlur={() => {
-								props.onBlur && props.onBlur() ;
+								props.onBlur && props.onBlur();
 							}}
 						></textarea>
 					) : (
@@ -68,12 +67,12 @@ function InputField({
 								props.onChange ? props.onChange(inputRef.current.value) : null;
 								validateInput();
 							}}
-						onBlur={() => {
-							props.onBlur && props.onBlur() ;
-						}}
+							onBlur={() => {
+								props.onBlur && props.onBlur();
+							}}
 						/>
 					)}
-					
+
 					{label ? (
 						<label
 							htmlFor={id}
@@ -85,26 +84,25 @@ function InputField({
 						</label>
 					) : null}
 					<span className={`flex gap-2 absolute ${error ? "p-2" : null}`}>
-            <p className={`text-red-500 text-xs`}>{error}</p>
-          </span>
+						<p className={`text-red-500 text-xs`}>{error}</p>
+					</span>
 				</div>
-				{addon && addon.right && (
+				{addon &&
+					addon.right &&
 					addon.right.map((item, index) => {
 						return (
-							<Button key={index}
-							        mode={"tertiary"}
-							        icon={item.icon}
-							        onClick={item.onClick}
-							>
-							
-							</Button>
-						)
-					})
-				)}
+							<Button
+								key={index}
+								mode={"tertiary"}
+								icon={item.icon}
+								onClick={item.onClick}
+							></Button>
+						);
+					})}
 			</div>
 			{button ? <Button {...button} onClick={handleSubmit}></Button> : null}
 		</div>
 	);
 }
 
-export default InputField
+export default InputField;
