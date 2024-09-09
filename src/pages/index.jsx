76d69131f093f10921013a4491ae61f2dc21991c
arnavgour <133200768+arnavgour@users.noreply.github.com>
2024-09-09 15:07:54 +0530

    index page and tooltip fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -152,7 +152,7 @@ export default function Component() {
                 </Card>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 <Card>
                     <CardHeader>
                         <CardTitle>Recent Activities</CardTitle>
