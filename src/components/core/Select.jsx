7ce7d4a170e294d279f5dd9593be999d7485d984
Sudhanshu Modi <sudhanshu565868@gmail.com>
2024-09-09 18:12:50 +0530

    now Select component taking full width

--- src/components/core/Select.jsx
+++ src/components/core/Select.jsx
@@ -1,156 +1,170 @@
-import {forwardRef, useEffect, useMemo, useState} from "react";
-import {type} from "@/lib/utils";
-import {toast} from "sonner";
+import { forwardRef, useEffect, useMemo, useState } from "react";
+import { type } from "@/lib/utils";
+import { toast } from "sonner";
 import axios from "axios";
-import {useMediaQuery} from "usehooks-ts"
+import { useMediaQuery } from "usehooks-ts";
 
-import {Popover, PopoverContent, PopoverTrigger} from "~/ui/popover";
-import {Button} from "~/ui/button";
-import {Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList} from "~/ui/command";
-import {Drawer, DrawerContent, DrawerTrigger} from "~/ui/drawer";
-import {Icon, Loader, Text} from "~";
+import { Popover, PopoverContent, PopoverTrigger } from "~/ui/popover";
+import { Button } from "~/ui/button";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from "~/ui/command";
+import { Drawer, DrawerContent, DrawerTrigger } from "~/ui/drawer";
+import { Icon, Loader, Text } from "~";
 import PropTypes from "prop-types";
 
-const Select = forwardRef((
-	{className, source, map = {key: "id", value: "name"}, defaultValue, ...props},
-	ref) => {
-	const [data, setData] = useState([]);
-	const isDesktop = useMediaQuery("(min-width: 768px)")
-	const [loading, isLoading] = useState(true);
-	const [selected, setSelected] = useState(null)
-	const [open, setOpen] = useState(false)
-	const [value, setValue] = useState("")
-	
-	
-	useMemo(() => {
-		switch (type(source)) {
-			
-			case "array":
-				setData(source)
-				break
-			case "object":
-				if (!source.type) toast("Source type not defined");
-				switch (source.type.toLowerCase()) {
-					case "lov":
-						axios
-							.get(`/dmsapi/cms/query/GetLOVIdNameList?lovType=${source.parameter}`)
-							.then((res) => {
-								console.log(res);
-								setData(res.data)
-								
-							});
-						break;
-					case "enum":
-						axios
-							.get(`/dmsapi/cms/query/GetEnumIdNameList?enumType=${source.parameter}`)
-							.then((res) => {
-								console.log(res);
-								setData(res.data)
-							});
-						break;
-					case "table":
-						axios
-							.get(`/dmsapi/cms/query/cms/query/TableData?tableName=${source.parameter}`)
-							.then((res) => {
-								console.log(res);
-								setData(res.data)
-							});
-				}
-				break;
-			case "string":
-				
-				axios.get(source)
-					.then((res) => {
-						if (!res.data) {
-							console.error("No data found")
-							setData([])
-						}
-						let sortedData = res.data.sort((a, b) => {
-							return a[map.id] > b[map.id];
-						})
-						setData(sortedData);
-					})
-					.catch((e) => {
-						console.log(e);
-						isLoading(false);
-					});
-			
-		}
-	}, [source]);
-	useMemo(() => {
-		if (defaultValue) {
-			let defaultKey = defaultValue
-			if (type(defaultValue) === "object") {
-				defaultKey = data.find((d) => d[map.value] === defaultValue[map.value])
-				
-			}
-			setSelected(defaultKey)
-		}
-	}, [data])
-	useEffect(() => {
-		isLoading(false)
-		
-	}, []);
-	
-	return (
-		loading ?
-			<Loader />
-			: (
-				isDesktop ?
-					<Popover open={open}
-					         onOpenChange={setOpen}>
-						<PopoverTrigger asChild>
-							<Button variant="outline"
-							        className="w-[200px] justify-between gap-2">
-								
-								<Text truncate={10}
-								      className={"opacity-80"}>{selected?.[map.value] || "Select"}</Text>
-								<Icon icon="chevron-down"
-								      className={`ml-4 shrink-0 ${open ? "rotate-180" : "rotate-0"}`} />
-							
-							</Button>
-						</PopoverTrigger>
-						<PopoverContent className="w-[200px] p-0"
-						                align="start">
-							{loading ? <Loader /> : <ContentList data={data}
-							                                     map={map}
-							                                     setOpen={setOpen}
-							                                     setSelected={setSelected} />}
-						</PopoverContent>
-					</Popover> :
-					<Drawer open={open}
-					        onOpenChange={setOpen}>
-						<DrawerTrigger asChild>
-							<Button variant="outline"
-							        className="w-[150px] justify-start">
-								{selected ? <>{selected[map.value]}</> : <>Select</>}
-							</Button>
-						</DrawerTrigger>
-						<DrawerContent>
-							{loading ? <Loader /> : <div className="mt-4 border-t">
-								<ContentList data={data}
-								             map={map}
-								             setOpen={setOpen}
-								             setSelected={setSelected} />
-							</div>
-							}
-						</DrawerContent>
-					</Drawer>
-			)
-	
-	
-	)
-	
-	
-})
+const Select = forwardRef(
+	(
+		{
+			className,
+			source,
+			map = { key: "id", value: "name" },
+			defaultValue,
+			...props
+		},
+		ref
+	) => {
+		const [data, setData] = useState([]);
+		const isDesktop = useMediaQuery("(min-width: 768px)");
+		const [loading, isLoading] = useState(true);
+		const [selected, setSelected] = useState(null);
+		const [open, setOpen] = useState(false);
+		const [value, setValue] = useState("");
 
-function ContentList({
-	                     setOpen,
-	                     setSelected,
-	                     data,
-	                     map,
-                     }) {
-	console.log(data)
+		useMemo(() => {
+			switch (type(source)) {
+				case "array":
+					setData(source);
+					break;
+				case "object":
+					if (!source.type) toast("Source type not defined");
+					switch (source.type.toLowerCase()) {
+						case "lov":
+							axios
+								.get(
+									`/dmsapi/cms/query/GetLOVIdNameList?lovType=${source.parameter}`
+								)
+								.then((res) => {
+									console.log(res);
+									setData(res.data);
+								});
+							break;
+						case "enum":
+							axios
+								.get(
+									`/dmsapi/cms/query/GetEnumIdNameList?enumType=${source.parameter}`
+								)
+								.then((res) => {
+									console.log(res);
+									setData(res.data);
+								});
+							break;
+						case "table":
+							axios
+								.get(
+									`/dmsapi/cms/query/cms/query/TableData?tableName=${source.parameter}`
+								)
+								.then((res) => {
+									console.log(res);
+									setData(res.data);
+								});
+					}
+					break;
+				case "string":
+					axios
+						.get(source)
+						.then((res) => {
+							if (!res.data) {
+								console.error("No data found");
+								setData([]);
+							}
+							let sortedData = res.data.sort((a, b) => {
+								return a[map.id] > b[map.id];
+							});
+							setData(sortedData);
+						})
+						.catch((e) => {
+							console.log(e);
+							isLoading(false);
+						});
+			}
+		}, [source]);
+		useMemo(() => {
+			if (defaultValue) {
+				let defaultKey = defaultValue;
+				if (type(defaultValue) === "object") {
+					defaultKey = data.find(
+						(d) => d[map.value] === defaultValue[map.value]
+					);
+				}
+				setSelected(defaultKey);
+			}
+		}, [data]);
+		useEffect(() => {
+			isLoading(false);
+		}, []);
+
+		return loading ? (
+			<Loader />
+		) : isDesktop ? (
+			<Popover open={open} onOpenChange={setOpen}>
+				<PopoverTrigger asChild>
+					<Button variant="outline" className="w-full justify-between gap-2">
+						<Text truncate={10} className={"opacity-80"}>
+							{selected?.[map.value] || "Select"}
+						</Text>
+						<Icon
+							icon="chevron-down"
+							className={`ml-4 shrink-0 ${open ? "rotate-180" : "rotate-0"}`}
+						/>
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className="w-full p-0" align="start">
+					{loading ? (
+						<Loader />
+					) : (
+						<ContentList
+							data={data}
+							map={map}
+							setOpen={setOpen}
+							setSelected={setSelected}
+						/>
+					)}
+				</PopoverContent>
+			</Popover>
+		) : (
+			<Drawer open={open} onOpenChange={setOpen}>
+				<DrawerTrigger asChild>
+					<Button variant="outline" className="w-[150px] justify-start">
+						{selected ? <>{selected[map.value]}</> : <>Select</>}
+					</Button>
+				</DrawerTrigger>
+				<DrawerContent>
+					{loading ? (
+						<Loader />
+					) : (
+						<div className="mt-4 border-t">
+							<ContentList
+								data={data}
+								map={map}
+								setOpen={setOpen}
+								setSelected={setSelected}
+							/>
+						</div>
+					)}
+				</DrawerContent>
+			</Drawer>
+		);
+	}
+);
+
+function ContentList({ setOpen, setSelected, data, map }) {
+	console.log(data);
 	return (
 		<Command>
 			<CommandInput placeholder="Search ..." />
@@ -162,9 +176,9 @@ function ContentList({
 							key={item[map.key]}
 							value={item[map.value]}
 							onSelect={(value) => {
-								setSelected(data.find((d) => d[map.value] === value))
-								
-								setOpen(false)
+								setSelected(data.find((d) => d[map.value] === value));
+
+								setOpen(false);
 							}}
 						>
 							{item[map.value]}
@@ -173,7 +187,7 @@ function ContentList({
 				</CommandGroup>
 			</CommandList>
 		</Command>
-	)
+	);
 }
 
 Select.propTypes = {
@@ -188,6 +202,6 @@ Select.propTypes = {
 		key: PropTypes.string,
 		value: PropTypes.string,
 	}),
-}
+};
 
-export default Select
+export default Select;
